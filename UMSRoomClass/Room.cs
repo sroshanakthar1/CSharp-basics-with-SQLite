d2aa82be63e29d72f5efadb24559913e6804520b
Roshan Akthar <101711265+sroshanakthar1@users.noreply.github.com>
2025-06-02 16:27:45 +0530

    Add files via upload

---
+++ UMSRoomClass/Room.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMSRoomClass
+{
+    internal class Room
+    {
+        private string roomID;
+        private string roomName;
+        private string roomType;
+
+        public string RoomID
+        {
+            get { return roomID; }
+            set { roomID = value; }
+        }
+
+        public string RoomName
+        {
+            get { return roomName; }
+            set { roomName = value; }
+        }
+
+        public string RoomType
+        {
+            get { return roomType; }
+            set { roomType = value; }
+        }
+        public Room(string id, string name, string type)
+        {
+            RoomID = id;
+            RoomName = name;
+            RoomType = type;
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Room: {RoomName}, Type: {RoomType}");
+        }
+    }
+}
