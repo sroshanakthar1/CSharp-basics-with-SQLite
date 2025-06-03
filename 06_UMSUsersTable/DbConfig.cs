ca3378fa3d7b91379274df5dbab55fd1cde6093a
Roshan Akthar <101711265+sroshanakthar1@users.noreply.github.com>
2025-06-03 16:15:31 +0530

    Add files via upload

---
+++ 06_UMSUsersTable/DbConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMSUsersTable
+{
+    internal static class DbConfig
+    {
+        private static string Connectionstring => "Data Source=unicomtic.db;Version=3;";
+
+        public static SQLiteConnection GetConnection()
+        {
+            SQLiteConnection conn = new SQLiteConnection(Connectionstring);
+            conn.Open();
+            return conn;
+        }
+    }
+}
---
+++ 06_UMSUsersTable/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace UMSUsersTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var conn = DbConfig.GetConnection())
+            {
+
+                string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Users (
+                        UserID INTEGER PRIMARY KEY,
+                        Username TEXT NOT NULL,
+                        Password TEXT NOT NULL,
+                        Role TEXT NOT NULL
+                    )";
+                using (var cmd = new SQLiteCommand(createTableQuery, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                string insertQuery = @"
+                    INSERT INTO Users (Username, Password, Role) VALUES 
+                    ('admin1', 'pass123', 'Admin'),
+                    ('john123', 'student1', 'Student')";
+                using (var cmd = new SQLiteCommand(insertQuery, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+
+                string selectQuery = "SELECT * FROM Users";
+                using (var cmd = new SQLiteCommand(selectQuery, conn))
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    Console.WriteLine("UserID | Username | Password | Role");
+                    Console.WriteLine("------------------------------------");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["UserID"],-6} | {reader["Username"],-8} | {reader["Password"],-8} | {reader["Role"]}");
+                    }
+                }
+            }
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
